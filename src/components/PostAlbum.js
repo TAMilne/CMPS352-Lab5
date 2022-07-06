32c72828545d8bce0b2fa0e74d1effb00286466c
Travis Milne <Travismilne8@gmail.com>
2022-07-06 13:33:40 -0600

    Initial Commit

---
+++ src/components/PostAlbum.js
@@ -0,0 +1,63 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+
+class PostAlbum extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         albums: []
+      }
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/photos')
+        .then(response => {
+            this.setState({albums: response.data})
+        })
+    }
+
+  render() {
+    const{albums} = this.state
+    return (
+      <div>
+        <h1>List of Albums</h1>
+        <table>
+            <tr>
+                <th>
+                    ID
+                </th>
+                <th>
+                    Title
+                </th>
+                <th>
+                    Thumbnail
+                </th>
+                <th>
+                    Delete
+                </th>
+            </tr>
+            {
+                albums.map(album => 
+                <tr>
+                    <td>
+                        {album.id}
+                    </td>
+                    <td>
+                        {album.id}
+                    </td>
+                    <td>
+                        {album.id}
+                    </td>
+                    <td>
+                        {album.id}
+                    </td>
+                </tr>)
+            }
+        </table>
+      </div>
+    )
+  }
+}
+
+export default PostAlbum
